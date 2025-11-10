31c24e32d99d67e3565594ae5c898fda7886269f
Azepuo <azepuo11@gmail.com>
2025-11-10 14:29:53 +0100

    suprission de venv pas utilse d'utilise et organisation des routes dans cette branch

--- app/app.py
+++ app/app.py
@@ -1,33 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# la brache mes rendez-vous pour patient routes
 # Route pour la page d'accueil
 @app.route('/')
 def accueil():
     return render_template('patient/accueil.html')
-
 # Route pour le dashboard
 @app.route('/dashboard')
 def dashboard():
     return render_template('patient/dashboard.html')
-
 # Route pour mes rendez-vous
 @app.route('/mes_rdv')
 def mes_rdv():
     return render_template('patient/mes_rdv.html')
-
 # Route pour le profil
 @app.route('/profile')
 def profile():
     return render_template('patient/profile.html')
-
 @app.route('/logout')
 def logout():
     return render_template('patient/logout.html')
 @app.route('/prise_rdv')
 def priverdv():
     return render_template('patient/prise_rdv.html')
+#fin de la brache mes rendez-vous pour patient routes
 
-# Lancer l'application Flask
+
+
 if __name__ == '__main__':
     app.run(debug=True)
