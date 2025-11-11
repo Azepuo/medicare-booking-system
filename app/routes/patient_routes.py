fe31c0c926af3e91eb01e15e4eb37aea1217029a
Azepuo <azepuo11@gmail.com>
2025-11-11 17:23:24 +0100

    update routes and templates

--- app/routes/patient_routes.py
+++
@@ -1,8 +0,0 @@
-# Fichier : app/routes/patient_routes.py
-
-from flask import Blueprint, render_template, session, redirect, url_for, flash
-# (Assurez-vous que le décorateur login_required est défini ou importé ici)
-# from .decorators import login_required 
-
-patient = Blueprint('patient', __name__)
-
