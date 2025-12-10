35917256b287706b87e1eb52a88eaed668fc2745
Azepuo <azepuo11@gmail.com>
2025-12-10 20:25:10 +0100

    nettoyage de branch

--- database/connection.py
+++ database/connection.py
@@ -10,8 +10,8 @@ def create_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
-            port=os.getenv('DB_PORT', '3307'),
-            database=os.getenv('DB_NAME', 'medicare_booking'),
+            port=os.getenv('DB_PORT', '3306'),
+            database=os.getenv('DB_NAME', 'db_m'),
             user=os.getenv('DB_USER', 'root'),
             password=os.getenv('DB_PASSWORD', '')
         )
