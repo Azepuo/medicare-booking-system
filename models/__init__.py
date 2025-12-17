d1d3cf990601b4e2407dc564c55660177d352923
Moumen Mariam <mariammoumen7@gmail.com>
2025-12-17 22:41:23 +0100

    chore: mise à jour du template de base et des modèles
    - base.html : template principal
    - __init__.py : initialisation des modèles

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,4 @@
-# models/__init__.py
-# Expose les modèles de façon stable et fournit un alias pour compatibilité
-from .rendezvous import Rendezvous
 from .patient import Patient
+from .rendezvous import Rendezvous
 from .medecin import Medecin
-
-# Compatibilité avec du code ancien qui importait "RendezVous" (avec V majuscule interne)
-# Ceci évite les ImportError si certains fichiers utilisent RendezVous au lieu de Rendezvous.
-RendezVous = Rendezvous
-
-__all__ = ['Rendezvous', 'RendezVous', 'Patient', 'Medecin']
+from .disponibilite import Disponibilite
